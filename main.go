ec82c24ac4f901cbf4bf885138c55941218036bf
gitlab.com/pepa65 <pepa65@passchier.net>
2025-12-21 11:58:31 +0700

    More framesets

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/pepa65/asciigif/frames"
 )
 
-var version = "0.13.0"
+var version = "0.13.2"
 var clientnumber = 0
 var repo = "github.com/pepa65/asciigif"
 var defaultFramerate = 70
